3c67d26668c55b6e495a43259ea7381c67b17f50
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-02-29 16:01:20 +0000

    more test scenarios added.

--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -7,6 +7,18 @@ describe Calculate do
     it "takes 1 and 2 and returns 3" do
       expect(calculator.add(1,2)).to eq 3
     end
+
+    it "takes -1 and -1 and returns -2" do
+      expect(calculator.add(-1, -1)).to eq -2
+    end
+
+    it "takes Float::INFINITY and 0 and returns Float::INFINITY" do
+      expect(calculator.add(Float::INFINITY, 0)).to eq Float::INFINITY
+    end
+
+    it "takes Float::INFINITY and Float::INFINITY and returns Float::INFINITY" do
+      expect(calculator.add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+    end
   end
 
   context '#subtract' do
